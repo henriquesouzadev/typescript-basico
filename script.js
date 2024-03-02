82018dccaa855f0b48697b092c56055d332cdb57
henriquesouzadev <henriquesouzadev@gmail.com>
2024-03-02 11:40:49 -0300

    generics

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 "use strict";
-// Estado dos elementos
-// menu inativo:
-// class="" em nav
-// aria-expanded="false" em button
-// aria-label="Abrir Menu" em button
-// menu ativo:
-// class="active" em nav
-// aria-expanded="true" em button
-// aria-label="Fechar Menu" em button
-const btnMobile = document.getElementById('btn-mobile');
-function toggleMenu(e) {
-    const nav = document.getElementById('nav');
-    const button = e.currentTarget;
-    if (button instanceof HTMLElement && nav) {
-        nav.classList.toggle('active');
-        const active = nav.classList.contains('active');
-        if (active) {
-            button.setAttribute('aria-expanded', 'true');
-            button.setAttribute('aria-label', 'Fechar Menu');
-        }
-        else {
-            button.setAttribute('aria-expanded', 'false');
-            button.setAttribute('aria-label', 'Abrir Menu');
-        }
-    }
+const numeros = [1, 2, 3, 4, 5, 6, 7];
+const frutas = ['maça', 'uva', 'pêra', 'laranja', 'limão'];
+function firstFive(lista) {
+    return lista.slice(0, 5);
 }
-btnMobile === null || btnMobile === void 0 ? void 0 : btnMobile.addEventListener('pointerdown', toggleMenu);
+console.log(firstFive(numeros));
