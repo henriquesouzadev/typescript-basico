f0ff59948692d59552024251fade0fdd149ec1c2
henriquesouzadev <henriquesouzadev@gmail.com>
2024-03-08 15:56:31 -0300

    keyof 2

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 "use strict";
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-function somarVendas(vendas) {
-    const total = vendas.reduce((total, venda) => {
-        return total + venda[1];
-    }, 0);
-    document.body.innerHTML += `<p>Total: ${total}</p>`;
+function checkInterface(obj, key) {
+    if (obj && typeof obj === 'object' && key in obj) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-async function getVendas() {
-    const response = await fetch('https://api.origamid.dev/json/vendas.json');
-    const json = await response.json();
-    somarVendas(json);
+async function fetchData(url) {
+    const base = 'https://api.origamid.dev/json';
+    const response = await fetch(base + url);
+    return await response.json();
 }
-getVendas();
+async function handleData() {
+    const jogo = await fetchData('/jogo.json');
+    if (checkInterface(jogo, 'desenvolvedora')) {
+        console.log(jogo.desenvolvedora);
+    }
+    const livro = await fetchData('/livro.json');
+    if (checkInterface(livro, 'autor')) {
+        console.log(livro.autor);
+    }
+}
+handleData();
--- script.ts
+++ script.ts
@@ -1,28 +1,42 @@
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-
-interface ProdutoDetalhes {
-  marca: string;
-  cor: string;
+interface Jogo {
+  nome: string;
+  ano: number;
+  desenvolvedora: string;
+  plataformas: string[];
 }
 
-type Venda = [string, number, string, ProdutoDetalhes]
-
-function somarVendas(vendas: Venda[]) {
-  const total = vendas.reduce((total, venda) => {
-    return total + venda[1]
-  }, 0)
-
-  document.body.innerHTML += `<p>Total: ${total}</p>`
+interface Livro {
+  nome: string;
+  ano: number;
+  autor: string;
+  paginas: number;
 }
 
-async function getVendas() {
-  const response = await fetch('https://api.origamid.dev/json/vendas.json')
-  const json = await response.json()
-
-  somarVendas(json)
+function checkInterface<Interface>(obj: unknown, key: keyof Interface): obj is Interface {
+  if (obj && typeof obj === 'object' && key in obj) {
+    return true
+  } else {
+    return false
+  }
 }
 
-getVendas()
+async function fetchData<T>(url: string): Promise<T> {
+  const base = 'https://api.origamid.dev/json'
+  const response = await fetch(base + url)
 
+  return await response.json()
+}
+
+async function handleData() {
+  const jogo = await fetchData('/jogo.json')
+  if (checkInterface<Jogo>(jogo, 'desenvolvedora')) {
+    console.log(jogo.desenvolvedora)
+  }
+
+  const livro = await fetchData<Livro>('/livro.json')
+  if (checkInterface<Livro>(livro, 'autor')) {
+    console.log(livro.autor)
+  }
+}
+
+handleData()
