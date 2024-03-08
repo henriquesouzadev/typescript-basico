5bd9fa5e6719f18583e256da1494d57c40795ba4
henriquesouzadev <henriquesouzadev@gmail.com>
2024-03-07 21:23:22 -0300

    tuples 1

--- script.js
+++ script.js
@@ -1,60 +1,21 @@
 "use strict";
-console.log(document.constructor);
-class Produto {
-    constructor(nome, preco) {
-        // public: é o padrão para qualquer uma
-        // private: só pode ser acessada dentro da classe
-        // protected: só pode ser acessada dentro da classe e da subclasse
-        this.tipo = 'produto';
-        this.nome = nome;
-        this.preco = preco;
-    }
-    getPreco() {
-        return Produto.transformarPreco(this.preco);
-    }
-    static transformarPreco(preco) {
-        return `R$ ${preco}`;
-    }
+const produto1 = ['Notebook', 2500];
+const produto2 = ['Notebook', 2500];
+if (typeof produto1[0] === 'string') {
+    console.log(produto1[0].toLowerCase());
 }
-const livro = new Produto('O Senhor dos Anéis', 300);
-console.log(livro.getPreco());
-class Quadrado {
-    constructor(medida) {
-        this.lados = 4;
-        this.medida = medida;
-    }
-    getPerimetro() {
-        return this.medida * this.lados;
-    }
-    getArea() {
-        return this.medida * this.medida;
-    }
-}
-class Circulo {
-    constructor(raio) {
-        this.PI = Math.PI;
-        this.raio = raio;
-    }
-    getPerimetro() {
-        return Math.round(2 * this.PI * this.raio * 100) / 100;
-    }
-    getArea() {
-        return Math.round(this.PI * (this.raio * this.raio) * 100) / 100;
-    }
-}
-const formas = [2, 32, 12, 3, 4, 20, 37, 9].map((n) => {
-    if (n < 15) {
-        return new Quadrado(n);
+const [nome, preco] = produto2;
+function getText(selector) {
+    const el = document.querySelector(selector);
+    if (el) {
+        return [el, el.innerText];
     }
     else {
-        return new Circulo(n);
+        return null;
     }
-});
-formas.forEach((forma) => {
-    if (forma instanceof Quadrado) {
-        console.log(forma.getArea());
-    }
-    if (forma instanceof Circulo) {
-        console.log(forma.getPerimetro());
-    }
-});
+}
+const button = getText('button');
+if (button) {
+    button[0];
+}
+console.log(button);
--- script.ts
+++ script.ts
@@ -1,76 +1,26 @@
-console.log(document.constructor)
+const produto1 = ['Notebook', 2500]
+const produto2: [string, number] = ['Notebook', 2500]
 
-class Produto {
-  // public: é o padrão para qualquer uma
-  // private: só pode ser acessada dentro da classe
-  // protected: só pode ser acessada dentro da classe e da subclasse
-  readonly tipo = 'produto'
-  nome: string
-  private preco: number 
-
-  constructor(nome: string, preco: number) {
-    this.nome = nome
-    this.preco = preco
-  }
-
-  getPreco() {
-    return Produto.transformarPreco(this.preco)
-  }
-
-  static transformarPreco(preco: number) {
-    return `R$ ${preco}`
-  }
+if (typeof produto1[0] === 'string') {
+  console.log(produto1[0].toLowerCase())
 }
 
-const livro = new Produto('O Senhor dos Anéis', 300)
+const [nome, preco] = produto2
 
-console.log(livro.getPreco())
+function getText(selector: string) {
+  const el = document.querySelector<HTMLElement>(selector)
 
-class Quadrado {
-  readonly lados = 4
-  medida: number
-
-  constructor(medida: number) {
-    this.medida = medida
-  }
-
-  getPerimetro() {
-    return this.medida * this.lados
-  }
-  getArea() {
-    return this.medida * this.medida
-  }
-}
-
-class Circulo {
-  readonly PI = Math.PI
-  raio: number
-
-  constructor(raio: number) {
-    this.raio = raio
-  }
-
-  getPerimetro() {
-    return Math.round(2 * this.PI * this.raio * 100) / 100
-  }
-  getArea() {
-    return Math.round(this.PI * (this.raio * this.raio) * 100) / 100
-  }
-}
-
-const formas = [2, 32, 12, 3, 4, 20, 37, 9].map((n) => {
-  if (n < 15) {
-    return new Quadrado(n)
+  if (el) {
+    return [el, el.innerText] as const
   } else {
-    return new Circulo(n)
+    return null
   }
-})
+}
 
-formas.forEach((forma) => {
-  if (forma instanceof Quadrado) {
-    console.log(forma.getArea())
-  }
-  if (forma instanceof Circulo) {
-    console.log(forma.getPerimetro())
-  }
-}) 
+const button = getText('button')
+
+if (button) {
+  button[0]
+}
+
+console.log(button)
