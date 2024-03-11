c0b985f0aa4965cde8eb545450446bd4223d22a8
henriquesouzadev <henriquesouzadev@gmail.com>
2024-03-11 17:09:55 -0300

    show slide

--- dist/Slide.js
+++ dist/Slide.js
@@ -1,12 +1,26 @@
 export class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.show(this.index);
+    }
+    hide(el) {
+        el.classList.remove('active');
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach(el => this.hide(el));
+        this.slide.classList.add('active');
     }
 }
